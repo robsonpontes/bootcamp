1434273ce7664ac3ae9929f0c1f52d6cb0be6a91
robsonpontes <robson.tecnologia@gmail.com>
2019-05-28 06:51:57 -0300

    Treino

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,35 +1,33 @@
-const express = require("express");
-const multerConfig = require("./config/multer");
-const upload = require("multer")(multerConfig);
+const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
-const routes = express.Router();
+const routes = express.Router()
 
-const authMiddleware = require("./app/middlewares/auth");
-const guestMiddleware = require("./app/middlewares/guest");
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
 
 routes.use((req, res, next) => {
-  res.locals.flashSucces = req.flash("success");
-  res.locals.flashError = req.flash("error");
+  res.locals.flashSucces = req.flash('success')
+  res.locals.flashError = req.flash('error')
 
-  next();
-});
+  return next()
+})
 
-routes.get("/", guestMiddleware, SessionController.create);
-routes.post("/signin", SessionController.store);
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', SessionController.store)
 
-routes.get("/signup", guestMiddleware, UserController.create);
-routes.post("/signup", upload.single("avatar"), UserController.store);
+routes.get('/signup', guestMiddleware, UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
-routes.use("/app", authMiddleware);
+routes.use('/app', authMiddleware)
 
-routes.get("/app/logout", SessionController.destroy);
+routes.get('/app/logout', SessionController.destroy)
 
-routes.get("/app/dashboard", (req, res) => {
-  console.log(req.session.user);
-  return res.render("dashboard");
-});
+routes.get('/app/dashboard', DashboardController.index)
 
-module.exports = routes;
+module.exports = routes
